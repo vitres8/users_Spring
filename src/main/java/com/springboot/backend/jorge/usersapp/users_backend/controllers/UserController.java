27dbaa6384c034f5be24f51c2c5bdece1d173674
vitres8 <vitres8@gmail.com>
2024-10-17 22:55:14 -0400

    Implementacion de validacion de los campos del formulario desde el Backend, usando Springboot validation

--- src/main/java/com/springboot/backend/jorge/usersapp/users_backend/controllers/UserController.java
+++ src/main/java/com/springboot/backend/jorge/usersapp/users_backend/controllers/UserController.java
@@ -3,10 +3,13 @@ package com.springboot.backend.jorge.usersapp.users_backend.controllers;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.backend.jorge.usersapp.users_backend.entities.User;
 import com.springboot.backend.jorge.usersapp.users_backend.services.UserService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PutMapping;
 
 @CrossOrigin(origins = { "http://localhost:4200" })
@@ -31,6 +37,7 @@ public class UserController {
     @GetMapping
     public List<User> list() {
         return service.findAll();
+        
     }
 
     @GetMapping("/{id}")
@@ -40,17 +47,26 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.OK).body(userOptional.orElseThrow());
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(Collections.singletonMap("error", "El usuario no se encontro por el id: " + id));
+                    .body(Collections.singletonMap("error ", "El usuario no se encontro por el id: " + id));
         }
     }
 
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody User user) {
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return validation(result);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+    public ResponseEntity<?> update(@Valid @RequestBody User user, BindingResult result, @PathVariable Long id) {
+
+        if (result.hasErrors()) {
+            return validation(result);
+        }
+
         Optional<User> userOptional = service.findById(id);
 
         if (userOptional.isPresent()) {
@@ -75,4 +91,11 @@ public class UserController {
         return ResponseEntity.notFound().build();
     }
 
+    private ResponseEntity<?> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), "El campo " + error.getField() + " " + error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
--- src/main/java/com/springboot/backend/jorge/usersapp/users_backend/entities/User.java
+++ src/main/java/com/springboot/backend/jorge/usersapp/users_backend/entities/User.java
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import static jakarta.persistence.GenerationType.*;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -14,11 +17,21 @@ public class User {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String lastname;
 
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 4, max = 12)
     private String username;
+
+    @NotBlank
     private String password;
 
     public Long getId() {
